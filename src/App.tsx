27902bf9780ba18ec8d0be31de8fe9169080d1bf
beeth0ven <beeth0vendev@163.com>
2019-07-16 14:21:19 +0800

    initail commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AppBar, Toolbar, Typography, Grid, Button, Paper, Box,  } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import data from "./data";
+
+const useStyle = makeStyles(theme => ({
+  gridContainer: {
+    margin: theme.spacing(4),
+    maxWidth: 800,
+  },
+  sectionHeader: {
+    marginTop: theme.spacing(4)
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+  }
+}))
+
 
 const App: React.FC = () => {
+  const classes = useStyle();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position='static' color='primary'>
+        <Toolbar>
+          <Typography variant='h5'>光合公寓 - hao123</Typography>
+        </Toolbar>
+      </AppBar>
+      <Box display='flex' justifyContent='center'>
+        <Grid container className={classes.gridContainer} spacing={2}>
+          {data
+            .map((section, i) => [
+              (
+                <Grid item xs={12} key={section.name} className={classes.sectionHeader}>
+                  <Paper className={classes.paper} elevation={0}><Typography variant='subtitle2'>{section.name}</Typography></Paper>
+                </Grid>
+              ),
+              ...section.items
+                .map((item, i) => (
+                  <Grid item key={item.name}>
+                    <Button target='_blank' variant='outlined' href={item.url} key={item.name} >{item.name}</Button>
+                  </Grid>
+                ))
+            ]
+            )}
+        </Grid>
+      </Box>
     </div>
   );
 }
---
+++ src/data.tsx
@@ -0,0 +1,102 @@
+
+
+
+export default [
+    {
+        name: '办公',
+        items: [
+            {
+                name: '阿里企业邮箱',
+                url: 'https://qiye.aliyun.com/alimail'
+            },
+            {
+                name: 'OA',
+                url: 'http://oa.ovuni.com'
+            }
+        ]
+    },
+    {
+        name: 'Android 开发者平台',
+        items: [
+            {
+                name: '360',
+                url: 'http://dev.360.cn'
+            },
+            {
+                name: '豌豆荚',
+                url: 'http://aliapp.open.uc.cn'
+            },
+            {
+                name: '华为',
+                url: 'https://developer.huawei.com/'
+            },
+            {
+                name: '安智',
+                url: 'http://dev.anzhi.com'
+            },
+            {
+                name: '小米',
+                url: 'https://dev.mi.com'
+            },
+            {
+                name: '应用宝',
+                url: 'https://open.qq.com/'
+            },
+            {
+                name: 'VIVO',
+                url: 'https://developer.vivo.com.cn'
+            },
+            {
+                name: 'OPPO',
+                url: 'https://open.oppomobile.com'
+            },
+            {
+                name: '百度',
+                url: 'https://app.baidu.com/'
+            },
+        ]
+    },
+    {
+        name: '研发',
+        items: [
+            {
+                name: 'Bug',
+                url: 'https://work.bugtags.cn/'
+            },
+            {
+                name: '蓝湖',
+                url: 'https://lanhuapp.com/'
+            },
+            {
+                name: 'Coding',
+                url: 'https://coding.net/'
+            },
+            {
+                name: 'GitLab',
+                url: 'https://gitlab.com/'
+            },
+        ]
+    },
+    {
+        name: '工具',
+        items: [
+            {
+                name: 'JSON 转 Modle',
+                url: 'https://app.quicktype.io/'
+            },
+            {
+                name: '二维码',
+                url: 'https://cli.im/'
+            },
+            {
+                name: 'JSON 美化',
+                url: 'http://codebeautify.org/jsonviewer#'
+            },
+            {
+                name: 'GitLab',
+                url: 'https://gitlab.com/'
+            },
+        ]
+    },
+    
+];
