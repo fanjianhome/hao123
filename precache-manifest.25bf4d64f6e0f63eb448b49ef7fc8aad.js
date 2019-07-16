b742fd39ab4b1246f171f62c6b3bc93dcd75b599
beeth0ven <beeth0vendev@163.com>
2019-07-16 14:29:35 +0800

    Updates

---
+++ precache-manifest.25bf4d64f6e0f63eb448b49ef7fc8aad.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fae0dcbbb6b28b91e010289f8a2128af",
+    "url": "/hao123/index.html"
+  },
+  {
+    "revision": "f89a830f3ae103a2231a",
+    "url": "/hao123/static/js/2.9663c1dc.chunk.js"
+  },
+  {
+    "revision": "53315ec04773d018707a",
+    "url": "/hao123/static/js/main.a071efee.chunk.js"
+  },
+  {
+    "revision": "59c35dd11b3eef6ae650",
+    "url": "/hao123/static/js/runtime~main.c3feed40.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.35b1927e5ae957c07de35b23902feafe.js"
+  "/hao123/precache-manifest.25bf4d64f6e0f63eb448b49ef7fc8aad.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hao123/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
